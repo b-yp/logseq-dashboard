dd9c6770b9e155a656eec4224d325f4995416c5f
baiyapeng <baiyapeng@kezaihui.com>
2023-03-26 01:19:54 +0800

    feat: 实现 pages blocks words currentPageBlocks currentPageWords 的查询

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React, { useRef } from "react";
-import { useAppVisible } from "./utils";
+import React, { useEffect, useRef, useState } from "react";
+import '@logseq/libs'
 
-function App() {
+import { useAppVisible } from "./utils";
+import './style.scss'
+
+const App: React.FC = () => {
   const innerRef = useRef<HTMLDivElement>(null);
   const visible = useAppVisible();
-  if (visible) {
-    return (
-      <main
-        className="backdrop-filter backdrop-blur-md fixed inset-0 flex items-center justify-center"
-        onClick={(e) => {
-          if (!innerRef.current?.contains(e.target as any)) {
-            window.logseq.hideMainUI();
-          }
-        }}
-      >
-        <div ref={innerRef} className="text-size-2em">
-          Welcome to [[Logseq]] Plugins!
-        </div>
-      </main>
-    );
+
+  const [words, setWords] = useState(0)
+  const [currentPageWords, setCurrentPageWords] = useState<number | null>(null)
+  const [pages, setPages] = useState(0)
+  const [blocks, setBlocks] = useState(0)
+  const [currnetPageBlocks, setCurrentPageBlocks] = useState<number | null>(null)
+
+  const getWords = async () => {
+    const blocks = await logseq.DB.datascriptQuery(`
+      [:find (pull ?b [*])
+        :where
+        [?b :block/uuid _]
+        [?b :block/content]
+      ]
+    `)
+
+    const count = blocks.map(i => i[0].content || '').reduce((pre, cur) => {
+      return pre + (cur?.length || 0)
+    }, 0)
+
+    setBlocks(blocks.length)
+    setWords(count)
   }
-  return null;
+
+  const getCurrentPageWords = async () => {
+    const currentPage = await logseq.Editor.getCurrentPage()
+    const uuid = currentPage?.uuid
+
+    if (!uuid) {
+      setCurrentPageWords(null)
+      setCurrentPageBlocks(null)
+      return
+    }
+
+    const blocks = await logseq.DB.datascriptQuery(`
+      [:find (pull ?b [*])
+        :where
+        [?p :block/uuid #uuid "${uuid}"]
+        [?b :block/page ?p]
+        [?b :block/content]
+      ]
+    `)
+
+    const count = blocks.map(i => i[0].content || '').reduce((pre, cur) => {
+      return pre + (cur?.length || 0)
+    }, 0)
+
+    setCurrentPageBlocks(blocks.length)
+    setCurrentPageWords(count)
+  }
+
+  const getPages = async () => {
+    // FIXME: 过滤掉 Logseq 默认的页面
+    const pages = await logseq.Editor.getAllPages()
+
+    setPages(pages?.length || 0)
+  }
+
+  const fn = async () => {
+    getWords()
+    getCurrentPageWords()
+    getPages()
+  }
+
+  useEffect(() => {
+    console.log('vivsible', visible)
+
+    if (visible) {
+      // 用 setTimeout 包裹的原因：让样式先渲染出来，然后才进行计算
+      setTimeout(() => {
+        fn()
+      }, 100)
+    }
+  }, [visible])
+
+  return (
+    <main
+      className="main"
+      onClick={(e: { target: any; }) => {
+        if (!innerRef.current?.contains(e.target as any)) {
+          console.log('ref',)
+          logseq.hideMainUI();
+        }
+      }}
+    >
+      <div ref={innerRef} className="dashboard">
+        {currentPageWords !== null && <p>当前页面字数: {currentPageWords}</p>}
+        {currnetPageBlocks !== null && <p>当前页面块数: {currnetPageBlocks}</p>}
+        <p>总字数: {words}</p>
+        <p>总页面数: {pages}</p>
+        <p>总块数: {blocks}</p>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import "@logseq/libs";
-
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
+import App from "./App";
 import { logseq as PL } from "../package.json";
 
-// @ts-expect-error
+import "./index.css";
+import "./style.scss"
+
 const css = (t, ...args) => String.raw(t, ...args);
 
 const pluginId = PL.id;
@@ -21,6 +21,40 @@ function main() {
       <App />
     </React.StrictMode>
   );
+
+  // TODO: 用来实时统计字数的，由于性能问题，暂时注释掉
+  // logseq.DB.onChanged(async (res) => {
+  //   if (res.blocks.length !== 1 || !res.blocks[0].content) {
+  //     return
+  //   }
+
+  //   const count = countOutline(res.blocks[0].content)
+
+  //   logseq.provideUI({
+  //     key: "dashboard-bottom-bar",
+  //     path: "#app-container",
+  //     template: `
+  //     <div
+  //       class="dashboard-bottom-bar"
+  //       style="
+  //         width: 100vw;
+  //         height: 30px;
+  //         padding: 0 65px;
+  //         position: fixed;
+  //         left: 0;
+  //         right: 0;
+  //         bottom: 0;
+  //         display: flex;
+  //         align-items: center;
+  //         justify-content: flex-end;
+  //         background-color: rgba(51, 57, 65, 0.8);
+  //       "
+  //     >
+  //       字数: ${count} 
+  //     </div>
+  //   `,
+  //   })
+  // })
 
   function createModel() {
     return {
